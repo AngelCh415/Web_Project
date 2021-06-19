2b862d1e67ae2619f76b24802184ca2434c6e134
iivanggarcia <iivanggarcia0@gmail.com>
2021-06-19 18:55:34 -0500

    adminManejarInventario front end

--- jscript/adminManejarInventario.js
+++ jscript/adminManejarInventario.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
     });
 
     $(".aumentarInventario").click(function(){
-        let correo = $(this).attr("data-correo");
+        let nombre = $(this).attr("data-nombre");
         $.confirm({
             title: '<h3>Cafetería ESCOM 2021</h3>',
             content: '' +
             '<form action="" class="formInventario">' +
             '<div class="form-group">' +
-            '<label><h5>Ingresa el número de productos a agregar</h5></label>' +
+            '<label><h5>Ingresa el número de materiales a agregar</h5></label>' +
             '<input type="text" placeholder="0" class="inventario form-control" required />' +
             '</div>' +
             '</form>',
@@ -22,50 +22,102 @@ $(document).ready(function(){
                     text: 'Agregar',
                     btnClass: 'btn-blue',
                     action: function () {
-                        var saldo = this.$content.find('.saldo').val();
-                        if(!saldo){
+                        var numInventario = this.$content.find('.inventario').val();
+                        if(!numInventario){
                             $.alert('Por favor ingresa un numero');
                             return false;
                         }
-                        $.confirm({ //preguntamos al usuario si realmente quiere eliminar
-                            title: '<h3>Cafetería ESCOM 2021</h3>',
-                            content: '¿Estás seguro de cargar el saldo?',
-                            buttons: {
-                                Si: function () {//si lo confirma vamos a esta función, el Si es el texto que pondrá en el botón
-                                    $.ajax({
-                                        url:"./saldoCliente_AX.php",
-                                        method:"post",
-                                        data:{correo:correo,saldo:saldo},
-                                        cache:false,
-                                        success:function(respAX){
-                                            let AX = JSON.parse(respAX);
-                                            let tipo;
-                                            if(AX.codigo == 1) 
-                                                window.location.href = "./saldoCliente.php";
-                                            else{
-                                                tipo = "red";
-                                                $.alert({
-                                                    title:"<h3>Cafetería ESCOM 2021</h3>",
-                                                    content:AX.msj,
-                                                    type:tipo,
-                                                    icon:"fas fa-info-circle fa-2x",
-                                                    boxWidth: "50%",
-                                                    useBootstrap: false,
-                                                    onDestroy:function(){
-                                                    if(AX.codigo == 2)
-                                                        window.location.href = "./saldoCliente.php";
-                                                    }
-                                                });
-                                            }
+                        $.ajax({
+                            url:"./adminAgregarInventario_AX.php",
+                            method:"post",
+                            data:{nombre:nombre,numInventario:numInventario},
+                            cache:false,
+                            success:function(respAX){
+                                let AX = JSON.parse(respAX);
+                                let tipo;
+                                if(AX.codigo == 1) 
+                                    window.location.href = "./adminManejarInventario.php";
+                                else{
+                                    tipo = "red";
+                                    $.alert({
+                                        title:"<h3>Cafetería ESCOM 2021</h3>",
+                                        content:AX.msj,
+                                        type:tipo,
+                                        icon:"fas fa-info-circle fa-2x",
+                                        boxWidth: "50%",
+                                        useBootstrap: false,
+                                        onDestroy:function(){
+                                        if(AX.codigo == 2)
+                                            window.location.href = "./saldoCliente.php";
                                         }
                                     });
-                                },
-                                No: function () {//si no pues no hacemos nada
-                                },
+                                }
                             }
                         });
-                        //$.alert('Se agregarán $' + saldo +' pesos');
-                        //window.location.href = "./correoSaldoCliente.php?correo="+correo+",saldo="+saldo;
+                    }
+                },
+                cancelar: function () {
+                    //close
+                },
+            },
+            onContentReady: function () {
+                // bind to events
+                var jc = this;
+                this.$content.find('form').on('submit', function (e) {
+                    // if the user submits the form by pressing enter in the field.
+                    e.preventDefault();
+                    jc.$$formSubmit.trigger('click'); // reference the button and click it
+                });
+            }
+        });
+    });
+
+    $(".disminuirInventario").click(function(){
+        let nombre = $(this).attr("data-nombre");
+        $.confirm({
+            title: '<h3>Cafetería ESCOM 2021</h3>',
+            content: '' +
+            '<form action="" class="formInventario">' +
+            '<div class="form-group">' +
+            '<label><h5>Ingresa el número de materiales a eliminar</h5></label>' +
+            '<input type="text" placeholder="0" class="inventario form-control" required />' +
+            '</div>' +
+            '</form>',
+            buttons: {
+                formSubmit: {
+                    text: 'Eliminar',
+                    btnClass: 'btn-blue',
+                    action: function () {
+                        var numInventario = this.$content.find('.inventario').val();
+                        if(!numInventario){
+                            $.alert('Por favor ingresa un numero');
+                            return false;
+                        }
+                        $.ajax({
+                            url:"./adminEliminarInventario_AX.php",
+                            method:"post",
+                            data:{nombre:nombre,numInventario:numInventario},
+                            cache:false,
+                            success:function(respAX){
+                                let AX = JSON.parse(respAX);
+                                let tipo;
+                                if(AX.codigo == 1) 
+                                    window.location.href = "./adminManejarInventario.php";
+                                else{
+                                    tipo = "red";
+                                    $.alert({
+                                        title:"<h3>Cafetería ESCOM 2021</h3>",
+                                        content:AX.msj,
+                                        type:tipo,
+                                        icon:"fas fa-info-circle fa-2x",
+                                        boxWidth: "50%",
+                                        useBootstrap: false,
+                                        onDestroy:function(){
+                                        }
+                                    });
+                                }
+                            }
+                        });
                     }
                 },
                 cancelar: function () {
