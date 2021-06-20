79b2981d2b7a180a55434d7bea9b0d0908b41ec8
iivanggarcia <iivanggarcia0@gmail.com>
2021-06-20 03:13:24 -0500

    add files update

--- pages/registroCliente_AX.php
+++ pages/registroCliente_AX.php
@@ -1,10 +1,17 @@
 <?php
+    $archivo = $_FILES["archivo"];
     $nombre = $_POST["namesignup"];
     $correo = $_POST["emailsignup"];
     $contrasena = md5($_POST["passwordsignup"]);
+    
+    date_default_timezone_set('America/Mexico_City');
+    $hoy = date("g-i a");
 
+    $nuevo_nombre = $correo.'_'.$hoy.'_';
 
-    ///*
+    $dirUploads = "./files/"; //ruta donde se pondrán los datos
+    $archUpload = $dirUploads.$nuevo_nombre.$archivo["name"]; //le vuelvo a cambiar el nombre y accedo a él
+
     $respAX_JSON = array();
     $conexion = mysqli_connect("localhost","root","","cafeteria2021");
     $sql = "INSERT INTO `cafeteria2021`.`usuarios` (`nombre`, `correo`, `contrasena`, `tipo`, `saldo`, `auditoria`) VALUES ('$nombre', '$correo', '$contrasena', '2', '0', NOW())";
@@ -16,7 +23,7 @@
         $respAX_JSON["msj"] = "<h3>Error, correo ya registrado, favor de intentarlo de nuevo :c </h3>";
     } else{
         $resultado = mysqli_query($conexion,$sql);
-        if(mysqli_affected_rows($conexion) == 1){
+        if(mysqli_affected_rows($conexion) == 1 && move_uploaded_file($_FILES["archivo"]["tmp_name"],"$archUpload")){
             $respAX_JSON["codigo"]= 1;
             $respAX_JSON["msj"] = "<h3>Gracias. Tu registro se realizó correctamente :) </h3>";
         } else{
@@ -25,6 +32,4 @@
         }
     }
     echo json_encode($respAX_JSON);
-    //*/
-
 ?>
