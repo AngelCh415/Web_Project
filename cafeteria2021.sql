fed26abc7eb65ce9be15b60004142e239c3741cc
guslg325 <thelvadam@outlook.com>
2021-06-19 22:35:17 -0500

    Updated DB
    Set carrito.id_pedido to autoincrement

--- cafeteria2021.sql
+++ cafeteria2021.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-06-2021 a las 03:05:56
+-- Tiempo de generación: 20-06-2021 a las 05:33:49
 -- Versión del servidor: 10.4.19-MariaDB
 -- Versión de PHP: 7.4.20
 
@@ -205,6 +205,12 @@ ALTER TABLE `usuarios`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  MODIFY `id_pedido` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `inventario`
