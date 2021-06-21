3ec0e325cf505be589f49da2d8b5fd160c5c5acc
guslg325 <thelvadam@outlook.com>
2021-06-20 23:53:26 -0500

    Added prevention for duplicate user emails

--- pages/config_usuario_AX.php
+++ pages/config_usuario_AX.php
@@ -2,8 +2,18 @@
     $conexion = mysqli_connect("localhost","root","","cafeteria2021");
     mysqli_set_charset($conexion,"utf8");
 
-    $sql = "UPDATE cafeteria2021.usuarios SET correo = '".$_POST["email"]."' WHERE (id_usuario = ".$_POST["usuario"].");";
-    mysqli_query($conexion,$sql);
+    $sql = "SELECT * FROM cafeteria2021.usuarios WHERE correo = '".$_POST["email"]."'";
+    $res = mysqli_query($conexion,$sql);
+    $row = mysqli_num_rows($res);
 
-    echo "Correo actualizado, se cerrará su sesión.";
+    if($row == 1){
+        //Ya existe alguien con ese correo
+        echo "Error, ese correo ya está registrado.";
+    }else{
+        //no existe
+        $sql = "UPDATE cafeteria2021.usuarios SET correo = '".$_POST["email"]."' WHERE (id_usuario = ".$_POST["usuario"].");";
+        mysqli_query($conexion,$sql);
+
+        echo "Correo actualizado, se cerrará su sesión.";
+    }
 ?>
